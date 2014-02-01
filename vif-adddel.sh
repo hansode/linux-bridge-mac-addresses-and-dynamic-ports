5c727905a94f126448e3d96db8e551340b6b469b
Masahito Yoshida <masahito@axsh.net>
2014-02-02 00:42:46 +0900

    add a result.

--- vif-adddel.sh
+++ vif-adddel.sh
@@ -14,22 +14,38 @@ declare brname=brtap
 function showmac() {
   cat /sys/class/net/${brname}/address
 }
- 
+
+total=0
+changed=0
+
 for i in {0..9}; do
-  tapname=${tapprefix}${i}
+  total=$((${total} + 1))
+  tapname=${tapprefix}
   before_mac=$(showmac)
 
-  echo "... ${i} ${before_mac}"
+  printf "... %02d %s" ${i} ${before_mac}
  
   # setup
   tunctl -t ${tapname} >/dev/null
  
   # main test
   brctl addif    ${brname} ${tapname}
-  after_mac=$(showmac); [[ "${before_mac}" == "${after_mac}" ]] || echo changed:addif ${before_mac} ${after_mac}
+  after_mac=$(showmac)
+  if [[ "${before_mac}" != "${after_mac}" ]]; then
+    printf " [addif] ${before_mac} -> ${after_mac}"
+    changed=$((${changed} + 1))
+  fi
   brctl delif    ${brname} ${tapname}
-  after_mac=$(showmac); [[ "${before_mac}" == "${after_mac}" ]] || echo changed:delif ${before_mac} ${after_mac}
+  after_mac=$(showmac)
+  if [[ "${before_mac}" != "${after_mac}" ]]; then
+    printf " [delif] ${before_mac} -> ${after_mac}"
+    changed=$((${changed} + 1))
+  fi
  
   # teardown
   tunctl -d ${tapname} >/dev/null
+
+  echo
 done
+
+echo total:${total} changed:${changed} percentage:$((100 * ${changed} / ${total}))%
