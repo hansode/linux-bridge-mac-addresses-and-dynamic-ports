e3d6a3699518224bdc8b74a17b2e4bfb3a0e35cb
Masahito Yoshida <masahito@axsh.net>
2014-02-02 01:30:28 +0900

    use parameters.

--- config.d/node02.sh
+++ config.d/node02.sh
@@ -6,8 +6,11 @@
 set -e
 set -x
 
+brname=brtap
+ethname=eth1
+
 # fake eth1
-[[ -d /sys/class/net/eth1 ]] || tunctl -t eth1
+[[ -d /sys/class/net/${ethname} ]] || tunctl -t ${ethname}
 
 # add eth1 to bridge
-[[ -d /sys/class/net/eth1/brport ]] || brctl addif brtap eth1
+[[ -d /sys/class/net/${ethname}/brport ]] || brctl addif ${brname} ${ethname}
--- config.d/node03.sh
+++ config.d/node03.sh
@@ -6,11 +6,14 @@
 set -e
 set -x
 
+brname=brtap
+ethname=eth1
+
 # fake eth1
-[[ -d /sys/class/net/eth1 ]] || tunctl -t eth1
+[[ -d /sys/class/net/${ethname} ]] || tunctl -t ${ethname}
 
 # add eth1 to bridge
-[[ -d /sys/class/net/eth1/brport ]] || brctl addif brtap eth1
+[[ -d /sys/class/net/${ethname}/brport ]] || brctl addif ${brname} ${ethname}
 
 # set static mac adress to linux bridge
-ip link set brtap address $(cat /sys/class/net/eth1/address) 
+ip link set ${brname} address $(cat /sys/class/net/${ethname}/address)
