b60b61ed28877ee5050235687cc8673404cb6930
Masahito Yoshida <masahito@axsh.net>
2014-02-26 14:33:25 +0900

    make sure to generate ifcfg files.

--- config.d/node02.sh
+++ config.d/node02.sh
@@ -6,11 +6,86 @@
 set -e
 set -x
 
+## functions
+
+function gen_ifcfg_path() {
+  local device=${1:-eth0}
+  local ifcfg_path=/etc/sysconfig/network-scripts/ifcfg
+
+  echo ${ifcfg_path}-${device}
+}
+
+function install_ifcfg_file() {
+  local ifname=${1:-eth0}
+
+  tee $(gen_ifcfg_path ${ifname}) </dev/stdin
+}
+
+function render_ifcfg_bridge() {
+  local ifname=${1:-br0}
+  shift; eval local "${@}"
+
+  cat <<-EOS
+	DEVICE=${ifname}
+	TYPE=Bridge
+	$([[ -z "${address}" ]] || echo "MACADDR=${address}")
+	BOOTPROTO=none
+	ONBOOT=yes
+	EOS
+}
+
+function render_ifcfg_tap() {
+  local ifname=${1:-tap0}
+  shift; eval local "${@}"
+
+  cat <<-EOS
+	DEVICE=${ifname}
+	TYPE=Tap
+	$([[ -z "${address}" ]] || echo "MACADDR=${address}")
+	BOOTPROTO=none
+	ONBOOT=yes
+	EOS
+}
+
+function install_ifcfg_bridge() {
+  local ifname=${1:-br0}
+  shift; eval local "${@}"
+
+  render_ifcfg_bridge ${ifname} "${@}" | install_ifcfg_file ${ifname}
+}
+
+function install_ifcfg_tap() {
+  local ifname=${1:-tap0}
+  shift; eval local "${@}"
+
+  render_ifcfg_tap ${ifname} "${@}" | install_ifcfg_file ${ifname}
+}
+
+function install_ifcfg_bridge_map() {
+  local ifname=${1:-br0}
+  shift; eval local "${@}"
+
+  render_ifcfg_bridge ${ifname} "${@}" | install_ifcfg_file ${ifname}
+
+  local slave_ifcfg_path=$(gen_ifcfg_path ${slave})
+  if [[ ! -f ${slave_ifcfg_path} ]]; then
+    return 0
+  fi
+
+  local bridge_entry="BRIDGE=${ifname}"
+  egrep -q -w "^${bridge_entry}" ${slave_ifcfg_path} || {
+    echo ${bridge_entry} >> ${slave_ifcfg_path}
+  }
+}
+
+##
+
 brname=brtap
 ethname=eth1
 
-# fake eth1
-[[ -d /sys/class/net/${ethname} ]] || tunctl -t ${ethname}
+install_ifcfg_tap                  ${ethname} address=
+/etc/init.d/network restart
 
-# add eth1 to bridge
-[[ -d /sys/class/net/${ethname}/brport ]] || brctl addif ${brname} ${ethname}
+install_ifcfg_bridge     ${brname}            address=
+install_ifcfg_bridge_map ${brname} slave=${ethname}
+/etc/init.d/network restart
